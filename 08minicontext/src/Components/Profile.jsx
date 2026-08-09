fb686141e7051f96a27572b9867bafc4fb21504d
riyasehgal codes <riyasehgalcodes@gmail.com>
2026-08-09 22:01:23 +0530

    lecture-13

---
+++ 08minicontext/src/Components/Profile.jsx
@@ -0,0 +1,15 @@
+import React, {useContext} from "react";
+import UserContext from "../Context/UserContext";
+
+function Profile() { 
+    const {user} = useContext(UserContext)
+    
+    if (!user) return <div> PLEASE LOGIN </div>
+
+    return (
+        <div>
+            WELCOME {user.username}
+        </div>
+    )
+}
+export default Profile 
---
+++ 08minicontext/src/Context/UserContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserContext = React.createContext() //will give a wrapper 
+
+export default UserContext; 
+
+//making a provider now - new file 

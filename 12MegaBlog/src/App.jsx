6000f124b9d1f1b6b5b9ac0c4d779fd38c167023
riyasehgal codes <riyasehgalcodes@gmail.com>
2026-08-24 22:11:18 +0530

    megaBlog

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <> 
+      <h2> ART NOUVEAU</h2>
+    </>
+  )
+}
+
+export default App

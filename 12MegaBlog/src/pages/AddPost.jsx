0be9ff1a5074717871ca60b747184a1160462f9b
riyasehgal codes <riyasehgalcodes@gmail.com>
2026-08-29 22:06:39 +0530

    pages

---
+++ 12MegaBlog/src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Post as AddPost } from '../components'
+import { Container } from "../components/container/Container"
+import { PostForm } from "../components/post-form/PostForm"
+
+export default function AddPost() {
+  return (
+    <div className='py-8'>
+        <Container>
+            <PostForm/>
+        </Container>
+    </div>
+  )
+}
